a1facacc2afc5995a2b0583dbf7bfb524467a2fe
James Pleger <jpleger@gmail.com>
2021-05-23 21:23:57 -0700

    updating package paths

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
     include_package_data=True,
     package_data={
         # Include skeleton files
-        "": ["skel/*"],
+        "": ["skel/*", "skel/static/*", "skel/templates/*"],
     },
     entry_points = {
         'console_scripts': ['yaml2resume=yaml2resume.cli:main'],
