8f50f94ad0d9469e32e4ad77c14ded60f92de1b2
James Pleger <jpleger@gmail.com>
2021-05-23 21:24:16 -0700

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="yaml2resume", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="James Pleger",
     author_email="jpleger@gmail.com",
     description="Take yaml files and convert them into a resume",
